bca441e5f59bdeb097a2de46b6d30a791c63b962
Luis Caballero <lacb0311@hotmail.com>
2022-04-20 11:54:37 -0500

    se agrego ejercicio de promedio del salon

--- exman.py
+++ exman.py
@@ -1,10 +1,22 @@
 import numpy as np
 
-matriz = np.random.random((5, 5))
+def matrizIdentidad():
+    matriz = np.random.random((5, 5))
 
-for i in range(5):
-    for j in range(5):
-        if i != j:
-            matriz[i, j] = 0
+    for i in range(5):
+        for j in range(5):
+            if i != j:
+                matriz[i, j] = 0
 
-print(matriz)
+    print(matriz)
+
+def promedioCalificaciones():
+    matriz = np.random.uniform(0,5, size=(7, 7))
+    promedio = matriz.mean()
+    print(f'El promedio del curso es: {promedio}')
+    #print(matriz)
+    res = matriz[matriz > promedio]
+    print(res)
+
+promedioCalificaciones()
+
